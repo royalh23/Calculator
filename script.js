91bd2652ba503f731aa6160bfd56c5ac38cf76f7
Royal <royalhuseyn902@gmail.com>
2023-09-30 14:38:37 +0400

    Make rounded numbers number

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ function displayOperator(e) {
   operatorSelected = true;
   if (secondNumber != null) {
     firstNumber = operate(operator, firstNumber, secondNumber);
-    if (!Number.isInteger(firstNumber)) firstNumber = firstNumber.toFixed(6);
+    if (!Number.isInteger(firstNumber)) firstNumber = +firstNumber.toFixed(6);
     operator = e.target.textContent;
     screenValue.textContent = `${firstNumber} ${operator} `;
   } else {
@@ -61,7 +61,7 @@ function displayOperator(e) {
 
 function displayResult(e) {
   result = operate(operator, firstNumber, secondNumber);
-  if (!Number.isInteger(result)) result = result.toFixed(6);
+  if (!Number.isInteger(result)) result = +result.toFixed(6);
   screenValue.textContent = result;
 }
 
